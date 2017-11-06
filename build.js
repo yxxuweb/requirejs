93c7051242dbde83e9b54e7d1b0c987baa56f5e9
yxxu <yxxu_web@sina.com>
2017-11-06 12:46:03 +0800

    修改

--- build.js
+++ build.js
@@ -2,22 +2,14 @@
  * Created by xuyixin on 2017/4/10.
  */
 ({
-    appDir: './',
-    baseUrl:'js',
+    appDir: './src',
+    baseUrl:'./js',
     dir:'./build',
-    paths: {
-        text: 'thirdLib/requirejs/text',
-        template: 'thirdLib/template',
-        jquery: 'thirdLib/jqueryjs/jquery-2.1.4',
-        bootstrap: 'thirdLid/jqueryjs/bootstrap.min'
-    },
-    shim: {
-        bootstrap: {
-            dep: ['jquery']
-        }
-    },
+//  optimize: 'none',
+    inlineText: false,
+    mainConfigFile: './src/js/requireConfig.js',
     modules: [{
-        name: 'module/content'
+        name: 'main'
     }],
     fileExclusingRegExp: /^(r|build)\.js$|^(.git)|^(.vscode)$/
 })
