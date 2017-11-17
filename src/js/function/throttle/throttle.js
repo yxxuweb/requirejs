14462b0424b6d1e81a7621fbedcb0815e16c2b3d
yxxu <yxxu_web@sina.com>
2017-11-17 11:24:00 +0800

    添加节流函数

---
+++ src/js/function/throttle/throttle.js
@@ -0,0 +1,21 @@
+define(function () {
+    function throttle() {
+        var timer = null;
+        var startTime = new Date();
+        return function(){
+            var context = this;
+            var endTime = new Date();
+            var resTime = endTime - startTime;
+            //判断大于等于我们给的时间采取执行函数;
+            if(resTime >= time){
+                method.call(context);
+                //执行完函数之后重置初始时间，等于最后一次触发的时间
+                startTime = endTime;
+            }
+        }
+    }
+
+    return {
+        throttle: throttle
+    }
+})
